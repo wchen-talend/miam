51b08a95312dd029e16edf185689eafb7d472f5b
GGu <ggu@talend.com>
2014-06-11 12:06:11 +0800

    Update Stomach.java

--- src/my/stomach/Stomach.java
+++ src/my/stomach/Stomach.java
@@ -17,6 +17,7 @@ public class Stomach {
 	public Stomach() {
 		foodsInside.add(new Pizza());
 		/* Exercise 2 : add your food */
+		foodsInside.add(new Pizza()); //I like Pizza
 	}
 	@Override
 	public String toString() {

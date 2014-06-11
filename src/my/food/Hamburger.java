bf9e9a6fee89733998976c50b551615d23430183
zwzhao-talend <zwzhao@talend.com>
2014-06-11 16:48:44 +0800

    tup7 commit

---
+++ src/my/food/Hamburger.java
@@ -0,0 +1,25 @@
+package my.food;
+
+import java.util.List;
+
+public class Hamburger  implements Food{
+
+	@Override
+	public List<String> getIngredients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCountryOrigin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getCalory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

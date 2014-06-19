f907b6b31693d6f2d8cc4df12542505cec8cf1c8
zwzhao-talend <zwzhao@talend.com>
2014-06-19 17:05:51 +0800

    branch2 commit

--- src/my/stomach/Stomach.java
+++ src/my/stomach/Stomach.java
@@ -35,7 +35,7 @@ public class Stomach {
 		//foodsInside.add(new DanDanMian("***","***","***"));
 		foodsInside.clear(); //^_^
 		foodsInside.add(new ChaShaoBao("^_^","sauce1","bread1"));
-		foodsInside.add(new ChaShaoBao("^_^","branch1","branch1"));
+		foodsInside.add(new ChaShaoBao("^_^","branch1_branch2","branch1_branch2"));
 		Customer custemer = new Customer();
 		custemer.haochibu();
 	}
